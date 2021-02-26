90ca80f004182b4202345e1c5c8ca0283350cc46
BranK <23649425+ItsBranK@users.noreply.github.com>
2021-02-26 12:02:27 -0500

    BakkesModInjectorCs v2.1.4

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -193,7 +193,7 @@ namespace BakkesModInjectorCs.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("v2.1.3")]
+        [global::System.Configuration.DefaultSettingValueAttribute("v2.1.4")]
         public string INJECTOR_VERSION {
             get {
                 return ((string)(this["INJECTOR_VERSION"]));
